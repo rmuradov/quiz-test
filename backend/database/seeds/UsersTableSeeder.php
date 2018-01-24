90ff06dc6d65337edb84b43c1f1da7a5584da49c
ruslan.muradov <ruslan.muradov@acropolium.com>
2018-01-24 11:58:42 +0200

    backend: DatabaseSeeder and UsersTableSeeder

---
+++ backend/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new App\User();
+        $user->name = 'admin';
+        $user->email = 'admin@admin.com';
+        $user->password = bcrypt('admin');
+        //$user->api_token = str_random(60);
+        $user->save();
+    }
+}
